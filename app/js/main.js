10fad0732b8cc22ac0e45b2f0d2bb571b497bcae
sarkaromar <ausantemonir@gmail.com>
2018-04-10 17:36:59 +0600

    Update Responsive issues

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
 let name = "Md. Monir hossain";
 console.log(name);
+
+$(document).ready(function(){
+    $('.res-bar').click(function(){
+        console.log("click")
+        $('.top-nav').slideToggle();
+    });
+});
